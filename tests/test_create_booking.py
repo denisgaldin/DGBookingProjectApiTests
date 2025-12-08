53dacecf662b7b7d87ec02596753804395ac6562
Denis <denisgaldin@mail.ru>
2025-12-08 16:49:58 +0300

    Написал автотест на метод CreateBooking

--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -7,7 +7,7 @@ from conftest import api_client, generate_random_booking_data
 
 @allure.feature('Test creating Booking')
 @allure.story('Positive: Creating booking with custom data')
-def test_creating_booking_with_custom_data(api_client):
+def test_creating_booking_with_custom_data(api_client, generate_random_booking_data):
     with allure.step("Подготовка данных для  создания брони"):
         booking_data = generate_random_booking_data
 
